0e59af8b613934b79fca94e26a2048e0d8a681a2
davidcrc <david_drcr@hotmail.com>
2021-06-09 11:53:59 -0500

    Decoraator basic example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TweetEditorExample from "./TweetEditorExample";
+import ColorfulEditorExample from "./ColorfulEditorExample";
+
+const Examples = [
+  {
+    title: "Decorator Example",
+    desc: "",
+    component: TweetEditorExample
+  },
+  {
+    title: "ColorfulEditorExample",
+    desc: "",
+    component: ColorfulEditorExample
+  }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={{ overflowY: "scroll", height: "100%" }}>
+        {Examples.map((e, idx) => {
+          return (
+            <div key={idx}>
+              <h3>
+                {idx + 1} - {e.title}
+              </h3>
+              {<e.component />}
+              {/* <div style={{ height: 1, backgroundColor: "black" }} /> */}
+              <hr></hr>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
